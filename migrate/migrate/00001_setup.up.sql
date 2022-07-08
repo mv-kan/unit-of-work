e3bf007b43f9812e84f3d2c24c80f45c0ddb0363
Michael Kan <michael.kan.mail@gmail.com>
2022-07-08 14:07:12 +0200

    add complexity to set up db scheme

--- migrate/migrate/00001_setup.up.sql
+++ migrate/migrate/00001_setup.up.sql
@@ -1,10 +1,15 @@
+CREATE TABLE IF NOT EXISTS users (
+    id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    fullname VARCHAR(255) NOT NULL
+);
 CREATE TABLE IF NOT EXISTS todos (
     id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    username VARCHAR(255) NOT NULL  references users(username) ON DELETE CASCADE 
 );
-
 CREATE TABLE IF NOT EXISTS subs (
     id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     name VARCHAR(255),
-    todo_id uuid references todos(id) ON DELETE CASCADE
+    todo_id uuid NOT NULL references todos(id) ON DELETE CASCADE
 );
