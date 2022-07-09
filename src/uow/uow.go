4e0549de540866f39ef17b49404a722273e2db5c
Michael Kan <michael.kan.mail@gmail.com>
2022-07-09 12:03:41 +0200

    implement unit of work

---
+++ src/uow/uow.go
@@ -0,0 +1,42 @@
+package uow
+
+import (
+	"github.com/mv-kan/unit-of-work/repo"
+	"gorm.io/gorm"
+)
+
+type UnitOfWorkStore interface {
+	Users() repo.UserRepo
+	Todos() repo.TodoRepo
+}
+
+type uowStore struct {
+	todos repo.TodoRepo
+	users repo.UserRepo
+}
+
+func (store *uowStore) Users() repo.UserRepo {
+	return store.users
+}
+func (store *uowStore) Todos() repo.TodoRepo {
+	return store.todos
+}
+
+type UnitOfWorkBlock func(store UnitOfWorkStore) error
+
+type UnitOfWork interface {
+	Do(fn UnitOfWorkBlock) error
+}
+
+type unitOfWork struct {
+	db *gorm.DB
+}
+
+func (uow *unitOfWork) Do(fn UnitOfWorkBlock) error {
+	tx := uow.db.Begin()
+	newStore := uowStore{
+		todos: repo.NewTodoRepo(tx),
+		users: repo.NewUserRepo(tx),
+	}
+	return fn(&newStore)
+}
